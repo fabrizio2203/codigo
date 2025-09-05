203fe3f23ee8d434c72f7f7a20367fca05356f1d
fabrizio2203 <alonsoguzm22@gmail.com>
2025-09-04 23:54:32 -0500

    Codigo actualizado

--- codigo.java
+++ codigo.java
@@ -1,39 +1,43 @@
 import java.util.Random;
 import javax.swing.JOptionPane;
 
-public class OrdenamientoSimple {
-    
-    public static void quickSort(int[] arr, int inicio, int fin) {
-        if (inicio < fin) {
-            int pivote = particion(arr, inicio, fin);
-            quickSort(arr, inicio, pivote - 1);
-            quickSort(arr, pivote + 1, fin);
-        }
-    }
-
-    private static int particion(int[] arr, int inicio, int fin) {
-        int pivote = arr[fin], i = inicio - 1;
-        for (int j = inicio; j < fin; j++) {
-            if (arr[j] <= pivote) {
-                i++;
-                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
-            }
-        }
-        int temp = arr[i + 1]; arr[i + 1] = arr[fin]; arr[fin] = temp;
-        return i + 1;
-    }
+public class OrdenamientoBurbuja {
 
     public static void main(String[] args) {
-        int n = 10000;
+        int n = 5;
         int[] datos = new int[n];
         Random rand = new Random();
-        for (int i = 0; i < n; i++) datos[i] = rand.nextInt(100000);
+
+        for (int i = 0; i < n; i++) {
+            datos[i] = rand.nextInt(100);
+        }
+
+        StringBuilder original = new StringBuilder("Arreglo original:\n");
+        for (int i = 0; i < n; i++) {
+            original.append(datos[i]).append(" ");
+        }
 
         long inicio = System.currentTimeMillis();
-        quickSort(datos, 0, datos.length - 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (datos[j] > datos[j + 1]) {
+                    int temp = datos[j];
+                    datos[j] = datos[j + 1];
+                    datos[j + 1] = temp;
+                }
+            }
+        }
+
         long fin = System.currentTimeMillis();
 
+        StringBuilder ordenado = new StringBuilder("\n\nArreglo ordenado:\n");
+        for (int i = 0; i < n; i++) {
+            ordenado.append(datos[i]).append(" ");
+        }
+
         JOptionPane.showMessageDialog(null,
-            "QuickSort completado en " + (fin - inicio) + " ms");
+            original.toString() + ordenado.toString() +
+            "\n\nTiempo: " + (fin - inicio) + " ms");
     }
 }
